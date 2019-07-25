c5885b226002d5fd1c7ae3b8a40b5e90b59e6c26
jiesen <jieqqq678@163.com>
2019-07-25 13:38:14 +0800

    commit

---
+++ Jiesen.Framework/Component/Jiesen.Cache/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Jiesen.Cache
+{
+    public class Class1
+    {
+    }
+}
---
+++ Jiesen.Framework/Jiesen.Contract/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jiesen.Contract
+{
+    public class BaseEntity
+    {
+        public long Id { get; set; }
+    }
+}
