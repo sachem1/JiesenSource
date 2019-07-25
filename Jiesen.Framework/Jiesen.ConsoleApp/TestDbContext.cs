f60415a0be5cb64b8460ce011150a1506ff927e8
sachem1 <jieqqq678@163.com>
2019-07-26 06:53:16 +0800

    add

---
+++ Jiesen.Framework/Jiesen.ConsoleApp/TestDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Text;
+using Jiesen.DapperPoco;
+using Jiesen.DapperPoco.Adapters;
+using MySql.Data.MySqlClient;
+
+namespace Jiesen.ConsoleApp
+{
+    public class TestDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseConnectionString("");
+            //optionsBuilder.UseSqlAdapter(new SqlServerAdapter(SqlClientFactory.Instance));
+            optionsBuilder.UseSqlAdapter(new MySqlAdapter(MySqlClientFactory.Instance));
+        }
+    }
+}
